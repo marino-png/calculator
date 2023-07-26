5030074aafba2ba99f25a19c51158dbc297c7684
marino-png <marino@calcagni.eu>
2023-07-26 13:40:37 +0100

    Successfully implemented and tested the new calculation method. Removed eval() and ensured a secure implementation. The calculator is now fully functional and ready for integration. Looking forward to merging this feature

--- index.js
+++ index.js
@@ -1,27 +1,91 @@
  document.addEventListener('DOMContentLoaded', function() {
     const screen = document.querySelector('.screen');
+    let firstNumber = null;
+    let secondNumber = null;
+    let result = null;
      
     const equalSimbol = document.querySelector('.equal');
     equalSimbol.addEventListener('click', () => {
-       const result = eval(calculator);
-       screen.textContent = result;
+        operation();
     });
 
-    let calculator = '';
+    function operation(){
+        secondNumber = getIntNumber(SecondStringNumber);
+       switch (operator){
+            case "+": result = add(firstNumber,secondNumber);break;
+            case "-": result = subtract(firstNumber,secondNumber);break;
+            case "*": result = multiply(firstNumber,secondNumber);break;
+            case "/": result = divide(firstNumber,secondNumber);break;
+       }
+        screen.textContent = result;
+        firstNumber = result;
+        secondNumber = null;
+        FirstStringNumber ='';
+        SecondStringNumber ='';
+    }
+
+    let FirstStringNumber = "";
+    let SecondStringNumber = "";
     const elementbtns = document.querySelectorAll('.element');
     elementbtns.forEach(buttons =>{
         buttons.addEventListener('click', () =>{
             let text = "";
             text = buttons.textContent;
-            screen.textContent += text
-            calculator += text;
+            if(firstNumber === null){
+                FirstStringNumber += text;
+            } else {
+                SecondStringNumber += text;
+            }
+            screen.textContent += text;
+        });
+    });
+
+    let operator = "";
+    const operetorbtn = document.querySelectorAll('.operator');
+    operetorbtn.forEach(buttons =>{
+        buttons.addEventListener('click', () =>{
+            let text = "";
+            text = buttons.textContent;
+            if (operator !== ""){
+                operation();
+                operator = text;
+            }
+            operator = text;
+            if(firstNumber === null){
+                firstNumber = getIntNumber(FirstStringNumber);
+            }
+            screen.textContent = '';
         });
     });
 
     const cleanButton = document.querySelector('.clean');
     cleanButton.addEventListener('click', () => {
         screen.textContent = "";
-        calculator = '';
         result  = null;
+        firstNumber = null;
+        secondNumber = null;
+        FirstStringNumber = "";
+        SecondStringNumber = "";
+        operator = "";
     });
+
+    function getIntNumber(String){
+        return parseFloat(String);
+    }
+
+    function add(num1,num2){
+        return num1 + num2;
+    }
+
+    function subtract(num1,num2){
+        return num1 - num2;
+    }
+
+    function multiply(num1,num2){
+        return num1 * num2;
+    }
+
+    function divide(num1,num2){
+        return num1 / num2;
+    }
 });
